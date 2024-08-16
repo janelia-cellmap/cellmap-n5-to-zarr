259232716fe8055b38e15e6e49abdab492ed96a6
Zubov <zubov452@gmail.com>
2024-08-15 23:55:37 -0400

    chunk_by_chunk writing with dask map

--- src/copy_data_chunk_by_chunk.py
+++ src/copy_data_chunk_by_chunk.py
@@ -1,26 +1,15 @@
-import zarr
-import os
-import click
-from numcodecs import Zstd
-from pathlib import Path
-import numpy as np
-from dask.distributed import Client
-from dask_jobqueue import LSFCluster
-from dask.distributed import LocalCluster
-import zarr_attrs_multiscales as to_ngff
-#import copy_data_chunk_by_chunk as cd
-#from copy_data import cluster_compute
-from typing import Literal, Tuple, Union
-import cellmap_layout as cml
-from dask.array.core import slices_from_chunks, normalize_chunks
-from toolz import partition_all
-from dask.distributed import wait
-
 import os
 import zarr
 import dask.array as da
-import time
 
+from dask.distributed import Client
+import time 
+import numpy as np
+from numcodecs import Zstd
+from typing import Literal, Tuple, Union
+from dask.array.core import slices_from_chunks, normalize_chunks
+from toolz import partition_all
+from dask.distributed import wait
 
 
 def save_chunk(
@@ -45,8 +34,6 @@ def copy_arrays_data(src_dest_info,
                      num_workers: int,
                      invert: bool,
                      comp):
-    client.cluster.scale(num_workers)
-
     for src_obj, dest_group in src_dest_info:
 
         # if isinstance(src_obj, zarr.core.Array):
@@ -98,69 +85,40 @@ def copy_arrays_data(src_dest_info,
                 # wait for all the futures to complete
                 result = wait(fut)
                 print(f'Completed {len(part)} tasks in {time.time() - start}s')
+                
+                
+                
+                
 
-
-
-@click.command()
-@click.option('--src', '-s', type=click.Path(exists = True))
-@click.option("--dest", '-d', type=click.Path())
-@click.option('--mtype', '-mt', default = "em", type=click.STRING)
-@click.option('--gtruth', '-gt', default = "", type=click.STRING)
-@click.option('--inf', '-i' , default = "", type=click.STRING)
-@click.option('--masks', '-m', default = "", type=click.STRING)
-@click.option('--lm', '-lm', default = "", type=click.STRING)
-@click.option('--num_workers', '-c', default = 200, type=click.INT)
-@click.option('--scheduler', '-s', default = "lsf", type=click.STRING)
-@click.option('--clevel', '-cl', default = 6, type=click.INT)
-@click.option('--max_dask_chunk_num', '-maxchnum' , default = 50000, type=click.INT)
-@click.option('--dry', default = False, type=click.BOOL)
-
-def cli(src, dest, mtype, gtruth, inf, masks, lm, num_workers, scheduler, clevel, max_dask_chunk_num, dry):
-    compressor = Zstd(level=clevel)
-
-    num_cores = 1
-    cluster = LSFCluster(
-        cores=num_cores,
-        processes=num_cores,
-        memory=f"{15 * num_cores}GB",
-        ncpus=num_cores,
-        mem=15 * num_cores,
-        walltime="48:00",
-        local_directory = "/scratch/$USER/"
-        )
-    
-    #cluster = LocalCluster()
-    client = Client(cluster)
-    with open(os.path.join(os.getcwd(), "dask_dashboard_link" + ".txt"), "w") as text_file:
-        text_file.write(str(client.dashboard_link))
-    print(client.dashboard_link)
-
-
-    #figure out the layout of an output .zarr file. 
-    recon_groups = cml.get_store_info(src, mtype, inference = inf, groundtruth = gtruth, masks = masks, lm = lm)
-
-    #copy groups and arrays info to an output zarr file.  
-    root_dest, src_dest_info, zs = cml.create_cellmap_tree(recon_groups, dest, compressor)
-    
-    #add ome ngff multiscale metadata, if applicable
-    for item in src_dest_info:
-        if isinstance(item[0], zarr.hierarchy.Group): 
-            dest_group = root_dest[item[1]]    
-            to_ngff.normalize_to_ngff(dest_group)
-    
-    #copy input .n5 arrays data to corresponding arrays in the output .zarr file.
-    
-    
-    if dry == False:
-        copy_arrays_data(src_dest_info,
-                            zs,
-                            client,
-                            num_workers,
-                            invert=False,
-                            comp=compressor)
         
-        #copy_arrays_data(src_dest_info, zs, max_dask_chunk_num, compressor)
-   
+                    
+                    
+                    
 
-if __name__ == '__main__':
-    cli()
+# def cluster_compute(scheduler, num_cores):
+#     def decorator(function):
+#         def wrapper(*args, **kwargs):
+#             if scheduler == "lsf":
+#                 num_cores = 30
+#                 cluster = LSFCluster( cores=num_cores,
+#                         processes=1,
+#                         memory=f"{15 * num_cores}GB",
+#                         ncpus=num_cores,
+#                         mem=15 * num_cores,
+#                         walltime="48:00",
+#                         death_timeout = 240.0,
+#                         local_directory = "/scratch/zubovy/"
+#                         )
+#                 cluster.scale(num_cores)
+#             elif scheduler == "local":
+#                     cluster = LocalCluster()
+
+#             with Client(cluster) as cl:
+#                 text_file = open(os.path.join(os.getcwd(), "dask_dashboard_link" + ".txt"), "w")
+#                 text_file.write(str(cl.dashboard_link))
+#                 text_file.close()
+#                 cl.compute(function(*args, **kwargs), sync=True)
+#         return wrapper
+#     return decorator
+
+
